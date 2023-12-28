597d6eb4961b05b7f710068ba753ca015d712270
paulocunha31 <pauloroberto_odc@hotmail.com>
2023-12-27 22:27:32 -0300

    adicionado moedas e troca do title

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 const convertButton = document.querySelector(".convert-button")
 const conversionButton = document.querySelector(".arrow-button")
-const currencyConverter = document.querySelector(".currency-converter")// primeiro select
-const currencySelect = document.querySelector(".currency-select")// segundo select
+const currencyConverter = document.querySelector(".currency-converter")
+const currencySelect = document.querySelector(".currency-select")
+const modal = document.getElementById("modal-warning")
+const textModal = document.getElementById("text-modal")
 
-// formata valor do input-currency
+
+
+// format input-currency value
 function formatValueInput(input) {
 
     let valor = input.value.replace(/[\D]+/g, '')
@@ -25,16 +29,15 @@ outDate.value = dateFormatted
 
 const convertValues = async () => {
     const inputCurrency = document.querySelector(".input-currency")
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // valor digitado
-    const currencyValueConverted = document.querySelector(".currency-value")// Outras moedas
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") 
+    const currencyValueConverted = document.querySelector(".currency-value")
 
-
-    //  formata valor em real com vírgula para volor americano com ponto
+// format real value with comma to American value with period
     const inputCurrencyValue = (inputCurrency.value.replace(',', '.'))
 
     const selectOne = currencyConverter.value
     const selectTwo = currencySelect.value
-    console.log(selectOne)
+
 
     await fetch(`https://open.er-api.com/v6/latest/${selectOne}`)
         .then(response => response.json())
@@ -43,80 +46,156 @@ const convertValues = async () => {
             const convertedAmount = (inputCurrencyValue * rate).toFixed(2);
 
 
-            //////////////////////////// Primeiro Select ///////////////////////////////////
-            if (currencyConverter.value == "BRL") {
-                currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", { // formata moeda
-                    style: "currency",
-                    currency: "BRL"
-                }).format(inputCurrencyValue, selectOne)
 
 
-            }
-
-            if (currencyConverter.value == "USD") { // Se o select estiver selecionado em dólar, entre aqui!
-                currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
-                    style: "currency",
-                    currency: "USD"
-                }).format(inputCurrencyValue, selectOne)
+            if (currencyConverter.value !== currencySelect.value) {
 
 
 
-            }
-
-            if (currencyConverter.value == "EUR") { // Se o select estiver selecionado em euro, entre aqui!
-                currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-de", {
-                    style: "currency",
-                    currency: "EUR"
-                }).format(inputCurrencyValue, selectOne)
-
-            }
+                //////////////////////////// First Select ///////////////////////////////////
+                if (currencyConverter.value == "BRL") {
+                    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", { // formata moeda
+                        style: "currency",
+                        currency: "BRL"
+                    }).format(inputCurrencyValue, selectOne)
 
 
+                }
 
-            if (currencyConverter.value == "GBP") { // Se o select estiver selecionado em bitcoin, entre aqui!
-                currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
-                    style: "currency",
-                    currency: "GBP"
-                }).format(inputCurrencyValue, selectOne)
-            }
-
-
-            ////////////////////// Segundo Select /////////////////////////////////////////
-
-            if (currencySelect.value == "BRL") {
-                currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", { // formata moeda
-                    style: "currency",
-                    currency: "BRL"
-                }).format(convertedAmount, selectTwo)
+                if (currencyConverter.value == "USD") { 
+                    currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "USD"
+                    }).format(inputCurrencyValue, selectOne)
 
 
 
-            }
+                }
+
+                if (currencyConverter.value == "EUR") { 
+                    currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-de", {
+                        style: "currency",
+                        currency: "EUR"
+                    }).format(inputCurrencyValue, selectOne)
+
+                }
 
 
-            if (currencySelect.value == "USD") { // Se o select estiver selecionado em dólar, entre aqui!
-                currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-                    style: "currency",
-                    currency: "USD"
-                }).format(convertedAmount, selectTwo)
+
+                if (currencyConverter.value == "GBP") { 
+                    currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "GBP"
+                    }).format(inputCurrencyValue, selectOne)
+                }
+
+                if (currencyConverter.value == "CHF") { 
+                    currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "CHF"
+                    }).format(inputCurrencyValue, selectOne)
+                }
+
+                if (currencyConverter.value == "CNY") { 
+                    currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "CNY"
+                    }).format(inputCurrencyValue, selectOne)
+                }
+
+                if (currencyConverter.value == "ARS") { 
+                    currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "ARS"
+                    }).format(inputCurrencyValue, selectOne)
+                }
+
+                if (currencyConverter.value == "AUD") { 
+                    currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "AUD"
+                    }).format(inputCurrencyValue, selectOne)
+                }
 
 
 
-            }
+                ////////////////////// Two Select /////////////////////////////////////////
 
-            if (currencySelect.value == "EUR") {
-                currencyValueConverted.innerHTML = new Intl.NumberFormat("en-de", {
-                    style: "currency",
-                    currency: "EUR"
-                }).format(convertedAmount, selectTwo)
+                if (currencySelect.value == "BRL") {
+                    currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", { // formata moeda
+                        style: "currency",
+                        currency: "BRL"
+                    }).format(convertedAmount, selectTwo)
 
-            }
 
-            if (currencySelect.value == "GBP") {
-                currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-                    style: "currency",
-                    currency: "GBP"
-                }).format(convertedAmount, selectTwo)
+
+                }
+
+
+                if (currencySelect.value == "USD") { 
+                    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "USD"
+                    }).format(convertedAmount, selectTwo)
+
+
+
+                }
+
+                if (currencySelect.value == "EUR") {
+                    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-de", {
+                        style: "currency",
+                        currency: "EUR"
+                    }).format(convertedAmount, selectTwo)
+
+                }
+
+                if (currencySelect.value == "GBP") {
+                    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "GBP"
+                    }).format(convertedAmount, selectTwo)
+                }
+
+                if (currencySelect.value == "CHF") { 
+                    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "CHF"
+                    }).format(convertedAmount, selectTwo)
+                }
+
+                if (currencySelect.value == "CNY") { 
+                    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "CNY"
+                    }).format(convertedAmount, selectTwo)
+                }
+
+                if (currencySelect.value == "ARS") { 
+                    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "ARS"
+                    }).format(convertedAmount, selectTwo)
+                }
+
+                if (currencySelect.value == "AUD") { 
+                    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "AUD"
+                    }).format(convertedAmount, selectTwo)
+                }
+
+
+
+
+            } else {
+
+                const text = currencyConverter[currencyConverter.options.selectedIndex].text
+
+                textModal.innerHTML = `Não é permitido converter, ${text} para, ${text}.`
+                let myModal = new bootstrap.Modal(modal)
+                myModal.show()
+
 
 
             }
@@ -142,19 +221,19 @@ function invertValue() {
     selectTextOne = selecTextTwo
     selecTextTwo = acc
 
-    var text = selectTextOne
+    const textOne = selectTextOne
     for (let i = 0; i < selectOne.options.length; i++) {
 
-        if (selectOne.options[i].text === text) {
+        if (selectOne.options[i].text === textOne) {
             selectOne.selectedIndex = i;
             break
         }
 
     }
 
-    var text = selecTextTwo
+    const textTwo = selecTextTwo
     for (let i = 0; i < selectTwo.options.length; i++) {
-        if (selectTwo.options[i].text === text) {
+        if (selectTwo.options[i].text === textTwo) {
             selectTwo.selectedIndex = i;
             break
         }
@@ -193,6 +272,30 @@ function changeCurrencyConverter() {
     if (currencyConverter.value == "GBP") {
         currency.innerHTML = "Libra Esterlina"
         currencyImageConverter.src = "./assets/libra.png"
+    }
+
+
+    if (currencyConverter.value == "CHF") {
+        currency.innerHTML = "Franco Suíço"
+        currencyImageConverter.src = "./assets/franco-suico.png"
+    }
+
+
+    if (currencyConverter.value == "CNY") {
+        currency.innerHTML = "Yuan Chinês"
+        currencyImageConverter.src = "./assets/yuan-chines.png"
+    }
+
+
+    if (currencyConverter.value == "ARS") {
+        currency.innerHTML = "Peso Argentino"
+        currencyImageConverter.src = "./assets/peso-argentino.png"
+    }
+
+
+    if (currencyConverter.value == "AUD") {
+        currency.innerHTML = "Dólar Australiano"
+        currencyImageConverter.src = "./assets/dolar-autraliano.png"
     }
 
     convertValues()
@@ -234,6 +337,29 @@ function chageCurrency() {
 
     }
 
+
+    if (currencySelect.value == "CHF") {
+        currencyName.innerHTML = "Franco Suíço"
+        currencyImage.src = "./assets/franco-suico.png"
+    }
+
+
+    if (currencySelect.value == "CNY") {
+        currencyName.innerHTML = "Yuan Chinês"
+        currencyImage.src = "./assets/yuan-chines.png"
+    }
+
+
+    if (currencySelect.value == "ARS") {
+        currencyName.innerHTML = "Peso Argentino"
+        currencyImage.src = "./assets/peso-argentino.png"
+    }
+
+
+    if (currencySelect.value == "AUD") {
+        currencyName.innerHTML = "Dólar Australiano"
+        currencyImage.src = "./assets/dolar-autraliano.png"
+    }
 
 
     convertValues()
